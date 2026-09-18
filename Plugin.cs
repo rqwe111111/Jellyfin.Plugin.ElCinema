6e726527b64a649edb72eee3ddeb08b874868290
rqwe111111 <rqweed1@gmail.com>
2026-09-19 00:27:35 +0300

    Add Plugin.cs

---
+++ Plugin.cs
@@ -0,0 +1,40 @@
+using System.Globalization;
+using Jellyfin.Plugin.ElCinema.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.ElCinema;
+
+public sealed class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+{
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+
+    public static Plugin? Instance { get; private set; }
+
+    public override string Name => Constants.PluginName;
+
+    public override string Description => "Arabic movie and series metadata from elCinema.com.";
+
+    public override Guid Id => Guid.Parse("2af4cef6-0133-4519-8e31-18ffadeaeb41");
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return
+        [
+            new PluginPageInfo
+            {
+                Name = Name,
+                EmbeddedResourcePath = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0}.Configuration.configPage.html",
+                    GetType().Namespace)
+            }
+        ];
+    }
+}
