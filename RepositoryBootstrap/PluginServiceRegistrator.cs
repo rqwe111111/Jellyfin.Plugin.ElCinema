a98a80846827432abf4bc16f6d47cecb74d40e92
rqwe111111 <rqweed1@gmail.com>
2026-09-19 01:27:37 +0300

    Add Repository Bootstrap: RepositoryBootstrap/PluginServiceRegistrator.cs

---
+++ RepositoryBootstrap/PluginServiceRegistrator.cs
@@ -0,0 +1,15 @@
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jellyfin.Plugin.RepositoryBootstrap;
+
+public sealed class PluginServiceRegistrator : IPluginServiceRegistrator
+{
+    public void RegisterServices(
+        IServiceCollection serviceCollection,
+        IServerApplicationHost applicationHost)
+    {
+        serviceCollection.AddHostedService<RepositoryBootstrapService>();
+    }
+}
