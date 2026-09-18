0d3196a2453507ce786b565024ee157b987d7466
rqwe111111 <rqweed1@gmail.com>
2026-09-19 00:27:30 +0300

    Add Constants.cs

---
+++ Constants.cs
@@ -0,0 +1,10 @@
+namespace Jellyfin.Plugin.ElCinema;
+
+public static class Constants
+{
+    public const string PluginName = "elCinema Metadata Full";
+    public const string ProviderName = "elCinema";
+    public const string ProviderId = "ElCinema";
+    public const string DefaultBaseUrl = "https://elcinema.com";
+    public const string Language = "ar";
+}
