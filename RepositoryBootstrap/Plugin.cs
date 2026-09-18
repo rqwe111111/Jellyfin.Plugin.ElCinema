d3d398ce5e0f2a5b0a6a12c8c712319cf4be009c
rqwe111111 <rqweed1@gmail.com>
2026-09-19 01:27:35 +0300

    Add Repository Bootstrap: RepositoryBootstrap/Plugin.cs

---
+++ RepositoryBootstrap/Plugin.cs
@@ -0,0 +1,41 @@
+using System.Globalization;
+using Jellyfin.Plugin.RepositoryBootstrap.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.RepositoryBootstrap;
+
+public sealed class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+{
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+
+    public static Plugin? Instance { get; private set; }
+
+    public override string Name => "Repository Bootstrap";
+
+    public override string Description =>
+        "Adds or updates a Jellyfin plugin repository automatically.";
+
+    public override Guid Id => Guid.Parse("a4e57b93-7a8c-4d9b-bf5a-73b3414d9a91");
+
+    public IEnumerable<PluginPageInfo> GetPages()
+    {
+        return
+        [
+            new PluginPageInfo
+            {
+                Name = Name,
+                EmbeddedResourcePath = string.Format(
+                    CultureInfo.InvariantCulture,
+                    "{0}.Configuration.configPage.html",
+                    GetType().Namespace)
+            }
+        ];
+    }
+}
